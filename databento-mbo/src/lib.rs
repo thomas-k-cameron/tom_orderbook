a4cdf5e8947edb35485be731410e5b3ed8e955c7
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-05-24 07:07:56 +0000

    update

---
+++ databento-mbo/src/lib.rs
@@ -0,0 +1,12 @@
+use dbn::record::MboMsg;
+use market_datatypes::Side;
+use tom_orderbook::{UniqueOrderId, OrderBook};
+
+
+pub struct MakerOrder {
+
+}
+
+impl From<MboMsg> for MakerOrder {
+    
+}
